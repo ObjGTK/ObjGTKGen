306568fbf902ffde4fc836faeb895634a1ce632c
Letterus <git@devbeejohn.de>
2024-01-14 16:58:04 +0100

    Update year

--- src/GIR/GIRReturnValue.h
+++ src/GIR/GIRReturnValue.h
@@ -1,7 +1,7 @@
 /*
  * SPDX-FileCopyrightText: 2015-2017 Tyler Burton <software@tylerburton.ca>
- * SPDX-FileCopyrightText: 2021-2022 Johannes Brakensiek <objfw@codingpastor.de>
- * SPDX-FileCopyrightText: 2015-2022 The ObjGTK authors, see AUTHORS file
+ * SPDX-FileCopyrightText: 2021-2024 Johannes Brakensiek <objfw@codingpastor.de>
+ * SPDX-FileCopyrightText: 2015-2024 The ObjGTK authors, see AUTHORS file
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
